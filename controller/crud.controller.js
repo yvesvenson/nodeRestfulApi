c20a4ee10c90328220f8584d8552e12762d9e8f9
yvesseansoft <yves@seansoftinc.com>
2022-02-06 23:12:25 +0800

    Set update and delete functionality accessible only for administrator

--- controller/crud.controller.js
+++ controller/crud.controller.js
@@ -1,4 +1,6 @@
 const crudModel = require('../model/crud.model');
+const userModel = require('../model/user.model');
+
 //CREATE
 const addData = async (req, res) => {
     //GATHER DATA
@@ -6,7 +8,8 @@ const addData = async (req, res) => {
         firstname: req.body.firstname,
         lastname: req.body.lastname,
         mobile: req.body.mobile,
-        email: req.body.email
+        email: req.body.email,
+        userId: req.params.userId
     });
     //SAVING DATA
     try{
@@ -42,33 +45,50 @@ const viewData = async (req, res) => {
 //UPDATEONE WILL UPDATE ONLY FOR ONE FIELD
 //UPDATEMANY WILL UPDATE FOR MANY EVEN FOR OTHER FIELD
 const updateData = async (req, res) => {
-    try{
-        const updatedData = await crudModel.updateMany(
-            { 
-                _id: req.params.id
-            },
-            { 
-                $set: { 
-                    firstname: req.body.firstname,
-                    lastname: req.body.lastname,
-                    mobile: req.body.mobile,
-                    email: req.body.email
-                }
-            });
-        res.status(200).json(updatedData);
-    }catch(err){
-        res.status(400).json(err);
+    //CHECK IF USERID IS EXIST
+    const user = await userModel.findOne({ userId: req.params.userId});
+
+    if(user.role == 'user'){
+        res.status(400).json({success: false});
+    }else if(user.role == 'administrator'){
+        //UPDATE RECORD
+        try{
+            const updatedData = await crudModel.updateMany(
+                { 
+                    _id: req.params.id
+                },
+                { 
+                    $set: { 
+                        firstname: req.body.firstname,
+                        lastname: req.body.lastname,
+                        mobile: req.body.mobile,
+                        email: req.body.email
+                    }
+                });
+            res.status(200).json(updatedData);
+        }catch(err){
+            res.status(400).json(err);
+        }
     }
 }
 
 //DELETE SPECIFIC
 const deleteData = async (req, res) => {
-    try{
-        await crudModel.deleteOne({ _id: req.params.id });
-        res.status(200).json({ success: true });
-    }catch(err){
-        res.status(400).json({ success: false, data: err });
+    //CHECK IF USERID IS EXIST
+    const user = await userModel.findOne({ userId: req.params.userId});
+
+    //IF USER OR ADMINISTRATOR
+    if(user.role == 'user'){
+        res.status(400).json({success: false});
+    }else if(user.role == 'administrator'){
+        try{
+            await crudModel.deleteOne({ _id: req.params.crudId });
+            res.status(200).json({ success: true });
+        }catch(err){
+            res.status(400).json({ success: false, data: err });
+        }
     }
+
 }
 
 
--- router/crud.router.js
+++ router/crud.router.js
@@ -2,14 +2,14 @@ const router = require('express').Router();
 const crudController = require('../controller/crud.controller');
 const verify = require('../config/verifyToken');
 
-router.post('/add', verify, crudController.addData);
+router.post('/add/:userId', verify, crudController.addData);
 
 router.get('/view', verify, crudController.viewAll);
 
 router.get('/view/:id', verify, crudController.viewData);
 
-router.patch('/update/:id', verify, crudController.updateData);
+router.patch('/update/:cruId/:userId', verify, crudController.updateData);
 
-router.delete('/:id', verify, crudController.deleteData);
+router.delete('/delete/:cruId/:userId', verify, crudController.deleteData);
 
 module.exports = router;
